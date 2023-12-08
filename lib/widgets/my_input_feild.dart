b043d455aae672a0c14eb437ec113292883cd7e3
Raheel Baloch <muhammadraheel6240@gmil.com>
2023-12-08 11:33:23 +0500

    Custom TextField

--- lib/widgets/my_input_feild.dart
+++ lib/widgets/my_input_feild.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 
 
 class MyInputField extends StatefulWidget {
